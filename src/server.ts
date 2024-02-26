4b3fe0ec314563aa0695ba27a6b7475585393957
Youssuf Radi <youssufradi@youssufs-air.speedport.ip>
2024-02-26 15:32:43 +0100

    Update env variables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import helmet from 'helmet';
@@ -9,7 +8,6 @@ import { openAPIRouter } from '@api-docs/openAPIRouter';
 import errorHandler from '@common/middleware/errorHandler';
 import rateLimiter from '@common/middleware/rateLimiter';
 import requestLogger from '@common/middleware/requestLogger';
-import { getCorsOrigin } from '@common/utils/envConfig';
 import { gameRouter } from '@modules/game/gameRouter';
 import { healthCheckRouter } from '@modules/healthCheck/healthCheckRouter';
 
@@ -19,10 +17,8 @@ dotenv.config({
 
 const logger = pino({ name: 'server start' });
 const app: Express = express();
-const corsOrigin = getCorsOrigin();
 
 // Middlewares
-app.use(cors({ origin: [corsOrigin], credentials: true }));
 app.use(helmet());
 app.use(rateLimiter);
 app.use(express.json());
